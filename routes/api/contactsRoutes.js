b754f49f7362801d410cf5c7530494aa390a36f0
Bad-Raider <stanislav.gusiev@gmail.com>
2023-07-29 14:52:53 +0300

    added midleware

--- routes/api/contactsRoutes.js
+++ routes/api/contactsRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import contactsActions from "../../models/contactcts.js";
-// import  HttpError  from "../../helpers/HttpError.js";
+import validateBody from '../../helpers/midleware.js';
 import schema from "../../Schema/index.js";
 const contactsRouter = Router();
 
@@ -25,69 +25,48 @@ contactsRouter.get('/:id', async (req, res, next) => {
   try {
     const id = req.params.id;
     const result = await getContactById(id);
-
     if (!result) {
       res.status(404)
       throw new Error(`message": "Not found`)
     }
-
     res.json(result);
-
   } catch (error) {
-    next(error);
+    next();
   }
 })
 
-contactsRouter.post('/', async (req, res, next) => {
-  
+contactsRouter.post('/',
+  validateBody(schema.addContactsSchema),
+  async (req, res, next) => {
   try {
-    
-    const  {error} = schema.addContactsSchema.validate(req.body);
-    if (error) {
-      res.status(400)
-      throw new Error(`message: missing required name field`)
-    };
-
     const newContact = await addContact(req.body);
     res.status(201).json(newContact);
-
   } catch (error) {
-    next(error)
+    next();
   }
 });
 
 contactsRouter.delete('/:id', async (req, res, next) => {
-  
   try {
     const id = req.params.id;
     const result = await removeContact(id);
-
     if (!result) {
       res.status(404);
       throw new Error("message: Not found");
     }
-
     res.json({ message: "contact deleted" })
-
   } catch (error) {
     next(error)
   }
-  
 });
 
-contactsRouter.put('/:id', async (req, res, next) => {
-  
+contactsRouter.put('/:id',
+  validateBody(schema.updateContactsSchema),
+  async (req, res, next) => {
   try {
-    const { error } = schema.updateContactsSchema.validate(req.body);
-    if (error) {
-      res.status(400);
-      throw new Error("message: missing required name field");
-    };
-
     const id = req.params.id;
     const result = await updateContact(id, req.body);
     res.json(result);
-
   } catch (error) {
     next(error)
   }
