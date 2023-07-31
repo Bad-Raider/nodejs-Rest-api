8d1c04e75cefb7d14a6e86cf25526d37fe11f9ca
Bad-Raider <stanislav.gusiev@gmail.com>
2023-07-31 13:36:29 +0300

    update & instal
    1. update server.js
    2. instal mongoose and doenv

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-import app from './app.js';
+import app from "./app.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+dotenv.config();
+
+const { DB_HOST } = process.env;
+
+// const DB_HOST = "mongodb+srv://Stanislav:0tAQg5N2DH4TAKl9@cluster0.kt4auyt.mongodb.net/my-contacts?retryWrites=true&w=majority"; 
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000")
+    });
+  })
+  .catch(error=>console.log(error.message))
 
