818d6eabed96f0d5c0bdda41c6eb8c27f79cc346
Bad-Raider <stanislav.gusiev@gmail.com>
2023-08-12 13:05:43 +0300

    start project

--- schemas/loginSchema.js
+++ schemas/loginSchema.js
@@ -3,8 +3,9 @@ import Joi from 'joi';
 const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 const loginSchema = Joi.object({
-password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')).required(),
-  email: Joi.string().pattern(emailRegexp).required(),});
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')).required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+});
 
 
 
